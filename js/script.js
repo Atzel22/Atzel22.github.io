02d240e85eb20aba8aca16d5f70b5860b6e99fa6
Atzel <gad_dalelo@hotmail.com>
2022-05-27 13:06:36 -0300

    Algunas modificaciones finales

--- js/script.js
+++ js/script.js
@@ -17,27 +17,25 @@ let enviarConsulta = function(event){
     let mail = document.querySelector('#input-mail').value;
     let consulta = document.querySelector('#input-consulta').value;
     let error3 = document.querySelector('#error3');
-    let exito = document.querySelector('#exito');
     if(nombre.length > 20){
-        // let error1 = document.querySelector('#error1');
         error3.innerHTML = 'La cantidad máxima de caracteres permitidos es 20.';
         error3.style.setProperty("border", "solid 1px red");
         return false;
     }
     if(!mail.includes('@')){
-        // let error2 = document.querySelector('#error2');
-        error3.innerHTML = 'El formato de mail no es válido.'
+        error3.innerHTML = 'El formato de mail no es válido.';
         error3.style.setProperty("border", "solid 1px red");
+        error3.style.setProperty("border-radius", "10px");
         return false;
     }
     if(consulta.length > 500){
-        error3.innerHTML = 'La cantidad máxima de caracteres permitidos es 500.'
+        error3.innerHTML = 'La cantidad máxima de caracteres permitidos es 500.';
         error3.style.setProperty("border", "solid 1px red");
         return false;
     }
-    error3.innerHTML = 'Tu consulta se envió correctamente.'
+    error3.innerHTML = 'Tu consulta se envió correctamente.';
     error3.style.setProperty("border", "solid 1px green");
-    error3.style.setProperty("border-radius", "10px")
+    error3.style.setProperty("border-radius", "10px");
 }
 
 
